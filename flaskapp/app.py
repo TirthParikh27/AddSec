99eef3620038c8d566afedbc351ed6816fc9d59f
mukul sharma <mukul@cyamast.com>
2021-07-27 12:23:23 +1000

    some changes before pulling

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+import yaml
 
 app = Flask(__name__)
 CORS(app)
 
+steps_list = []
+
 @app.route("/")
 def helloWorld():
   return "Hello, cross-origin-world!"
+
+@app.route("/pipeline")
+def secure():swwww
+
 
 if __name__ == '__main__':
   app.run(debug=True)
