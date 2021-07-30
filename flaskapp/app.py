8668872be133314db35fbcec255c2fbdb05ab925
mukul sharma <mukul@cyamast.com>
2021-07-31 01:16:16 +1000

    generate steps json and add configs for xap and snyk

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -118,13 +118,83 @@ def setPos():
 # {'id':{'data':{'label'}}}
 @app.route('/setToolNames', methods=['POST'])
 def makeSecure():
+    global secure_flow
+    global file_dict
+    tmp_pos_dict = {}
+    tool_pos_dict = {}
+    step_list = []
+    data = request.get_json()
+    # print(data)
+    for row in data:
+        try:
+            if 'dndnode' in row['id']:
+                try:
+                    tmp_pos_dict[row['id']]=row['data']['label']
+                except:
+                    print('label??')
+        except:
+            print("NO SOURCE")
+    for t in tmp_pos_dict:
+        for r in data:
+            try:
+                if t == r['source']:
+                    tool_pos_dict[r['target']] = tmp_pos_dict[t]
+                elif t == r['target']:
+                    tool_pos_dict[r['source']] = tmp_pos_dict[t]
+            except:
+                continue
+    for i in data:
+        try:
+            if isinstance(int(i['id']), int):
+                step_list.append(i['data']['label'])
+            else:
+                continue
+            if i['id'] in tool_pos_dict:
+                step_list.append(tool_pos_dict[i['id']])
+        except:
+            continue
+    steps = file_dict['jobs']['deploy']['steps']
+    l = []
+    names = []
+    for i in steps:
+        if i['name'] in step_list:
+            l.append(i)
+            names.append(i['name'])
+        elif 'slug' in i['name'] or 'checkout' in i['name']:
+            l.append(i)
+            names.append(i['name'])
+    print(l)
+    for j, s in enumerate(step_list):
+        if s not in names:
+            inlist = step_list[j-1]
+            if 'codeguru' in s.lower():
+                l.insert(names.index(inlist)+1,{"name":s, "uses": "aws@uses"})
+            elif 'zap' in s.lower():
+                l.insert(names.index(inlist)+1,{"name": s, "uses": "zap@uses", "with":"zap@with"})
+            elif 'docker' in s.lower():
+                l.insert(names.index(inlist)+1,{"name": s, "uses": "docker@uses", "with":"docker@with"})
+
+    # for i in step_list:
+    #     found = 0
+    #     for s in steps:
+    #         if s['name'] == i:
+    #             l.append(s)
+    #             found = 1
+    #     if found == 0:
+    #         if 'codeguru' in i.lower():
+    #             l.append({"name":i, "uses": "aws@uses"})
+    #         elif 'zap' in i.lower():
+    #             l.append({"name": i, "uses": "zap@uses", "with":"zap@with"})
+    #         else:
+    #             l.append(s)
+    print(l)
+    print(tool_pos_dict)
+    # print(step_list)
     secure_flow = {}
     data = request.get_json()
-    print(data)
+    # print(data)
     response={"res" : "Successfully integrated tools"}
     return response , 200
-
-
 
 
 if __name__ == "__main__":
