a985f0ea77c39a061f0b9adfe697dccfc6c92b8e
Tirth <tirthparikh27@gmail.com>
2021-07-30 20:05:42 +1000

    pull push functionality added

--- flaskapp/autoGit.py
+++ flaskapp/autoGit.py
@@ -1,22 +1,21 @@
 import git
 import os
 
-def autoGit():
+def cloneGit(url , dirName):
   # Check out via HTTPS
-  repo = git.Repo.clone_from('https://github.com/TirthParikh27/AddSec' , 'AddSec')
+  #repo = git.Repo.clone_from('https://github.com/TirthParikh27/AddSec' , 'AddSec')
+  repo = git.Repo.clone_from(url , dirName)
   #repo = git.Repo('AddSec') 
   origin = repo.remote("origin")  
-  print(repo)
-  # Edit files
-  f = open(os.path.abspath(os.getcwd())+"\AddSec\demofile.txt", "w")
-  f.write("Script works ! We can edit and push workflow files")
-  f.close()
 
+def pushGit(filePath , msg , dirName):
+  repo = git.Repo(dirName) 
   # Commit
-  repo.index.add([os.path.abspath(os.getcwd())+'\AddSec\demofile.txt'])  # in this case filename would be "/User/some_user/some_dir/demofile.txt"
-  repo.index.commit("Workflow edited - test 3")
+  #repo.index.add([os.path.abspath(os.getcwd())+'\AddSec\demofile.txt'])  # in this case filename would be "/User/some_user/some_dir/demofile.txt"
+  repo.index.add([os.path.abspath(os.getcwd())+filePath]) 
+  #repo.index.commit("Workflow edited - test 3")
+  repo.index.commit(msg)
 
   # Push
   repo.git.push("--set-upstream", origin, repo.head.ref)
 
-autoGit()
